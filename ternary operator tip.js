d59f5d39c872eeee12cd860657ce39a16105a255
Sarah Feijó <sarah2504ana@gmail.com>
2026-06-28 14:01:08 -0300

    Refactor tip calculation and add input loop
    Refactor tip calculation into a separate function and add loop for multiple bill entries.

--- ternary operator tip.js
+++ ternary operator tip.js
@@ -1,8 +1,30 @@
-let bill = Number(prompt("Bill value: U$$ "));
+let bills = [];
+let tips = [];
+let bill;
 let tip;
+let totalValue;
 
-bill >= 50 && bill <= 300 ? tip = bill * 0.15 : tip = bill * 0.2;
-
-let totalValue = bill + tip;
-
-console.log("The bill was", bill.toFixed(2) +", the tip was", tip.toFixed(2) +", and the total value", totalValue.toFixed(2));
+while (true) {
+    bill = Number(prompt("Bill value: U$$ "));
+    
+    if (bill == 0) {
+        break;
+    }
+    
+    function calcTips (bill) {
+        if (bill >= 50 && bill <= 300) {
+            return bill * 0.15;
+        }
+        return bill * 0.2;
+    }
+    
+    tip = calcTips(bill);
+    totalValue = bill + tip;
+    
+    bills[bills.length] = bill;
+    tips[tips.length] = tip;
+    
+    console.log("The bill was", bill.toFixed(2) +", the tip was", tip.toFixed(2) +", and the total value", totalValue.toFixed(2));
+}
+bills[bills.length] = tips;
+console.log(bills);
